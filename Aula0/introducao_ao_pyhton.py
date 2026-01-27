73a0ef251d314c875a92e8312ed72e573cdd4a24
Bruno <brunohenriquebalsan089@gmail.com>
2026-01-27 19:54:22 -0300

    Projeto Alura - Imersão Dados com Python

---
+++ Aula0/introducao_ao_pyhton.py
@@ -0,0 +1,32 @@
+#Padrão de escrita do Pyhton snake_case
+
+nome = "Bruno"
+cor_favorita = "azul"
+
+print(f'A cor favorirta de {nome} é {cor_favorita}')
+
+#Atribuição de valores nuéricos
+
+saldo = 5000
+saldo = saldo + 2000
+
+print(f'O saldo de{nome} é de R$', saldo)
+
+#puxar ou uma solução feita por outro time ou pessoa
+
+import this
+
+#legibilidade dos codigos
+
+a = 250 #não entendivel
+
+tenis_nike = 250 #abordagem mais clara
+
+#Criação de condições
+
+idade = 16
+
+if idade >= 18:
+    print(f'{nome} já tem idade suficiente para dirigir')
+else:
+    print(f'{nome} ainda não tem idade para dirigir')
